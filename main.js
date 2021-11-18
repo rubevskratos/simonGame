8ac5098db04a8a71b0ae0c55f0769be7b37d1c30
Ruben Carballo <rcarballoq@gmail.com>
2021-11-18 19:05:17 +0100

    Added versus mode

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var gameIsOn = false;
 var soundsStatus = true;
 
+
+//Classic Simon Board Game with extra dificulty based on a totally different sequence every time.
 function SimonGame() {
     //game variables
     var self = this;
@@ -120,6 +122,127 @@ function SimonGame() {
 }
 
 
+//Versus edition!
+function SimonVersus() {
+    //This game will start with a sequence of 1 introduced by player 1, which player 2 need to respond in the same way.
+    //If success, then it's player 2's turn to introduce the lvl 1 sequence. And for player 1 to answer. 
+    //if Both succeed, then level will increase by 1 and both players will need to correctly answer his opponent's sequence until one of them fail.
+    var self = this;
+    var level = 1;
+    var playerTurn = false; //false - Player 1, true - Player 2
+    var bothPlayed = false;
+    var sequence = [];
+    var countUserInput = 0;
+    var buttons = document.getElementsByClassName('color');
+    var counter = document.getElementById('counter')
+    var thisTurn;
+    
+    this.resetCounters = function() {
+        sequence = [];
+        countUserInput = 0;
+        console.log(sequence);
+        console.log(countUserInput);
+    }
+    
+    var sounds = {
+        green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+        red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+        blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+        yellow: new Audio ("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+        intro: new Audio('media/sound/intro.mp3')
+    };
+
+    this.playSound = function(color) {
+        var thisSound = color;
+        
+        sounds[thisSound].currentTime=0;
+        sounds[thisSound].play();
+      };
+
+    this.getPlayerSequence = function() {
+        //Get the sequence.
+        sequence.push(this.getAttribute('id'));
+
+        if(soundsStatus === true) 
+         self.playSound(this.getAttribute('id'))
+
+        //Finalizes the Sequence when it's equally large than current level, and shows alternate player turn.
+        if (sequence.length === level) {
+            playerTurn = !playerTurn;
+            thisTurn = playerTurn === true ? 2 : 1;
+            counter.innerHTML = `<p>Player ${thisTurn} Answer.<br>Level ${level}</p>`
+            //Clears listeners and enables new listener on user output.
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].removeEventListener('click',self.getPlayerSequence);
+                buttons[i].addEventListener('click',self.checkPlayerAnswer)
+            }
+        } 
+    }
+
+    this.checkPlayerAnswer = function() {
+        if(soundsStatus === true) 
+            self.playSound(this.getAttribute('id'))
+
+        if (this.getAttribute('id') !== sequence[countUserInput]) {
+            self.gameIsOver();
+            return;
+        }
+
+        countUserInput++;
+
+        if (countUserInput === sequence.length) {
+            if (bothPlayed === false) {
+                self.nextStage();
+            } else {
+                level++;
+                thisTurn = 1;
+                self.nextStage();
+            }
+        }
+    }
+
+    this.nextStage = function() {
+        //              Clears sequence and counters.
+        self.resetCounters();
+        //              finalizes sequence check.
+        playerTurn = !playerTurn;
+        //              Alternate user turn.
+        counter.innerHTML = `<p>Player ${thisTurn} Turn.<br>Level ${level}</p>`
+
+        bothPlayed = !bothPlayed;
+        //              Clears listeners and enables new listener for user input.
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].removeEventListener('click',self.checkPlayerAnswer);
+            buttons[i].addEventListener('click',self.getPlayerSequence);
+        }
+    }
+    
+    this.init = function() {
+        //Shows First Player Turn Text.
+        counter.innerHTML = `<p>Player 1 Turn.<br>Level 1</p>`
+        
+        if(soundsStatus === true) 
+            this.playSound('intro')
+        //Initializes buttons to listen to Player Input.
+
+        for (var i = 0; i < buttons.length; i++)
+            buttons[i].addEventListener('click',self.getPlayerSequence)
+    }
+
+    this.showGameOver = function() {
+        let board = document.getElementById('board')
+        
+        let gameOver = document.createElement('div')
+        gameOver.setAttribute('id', 'gameOver');
+        gameOver.innerHTML = '<h1>GAME OVER!</h1>'
+        board.appendChild(gameOver);
+    }
+
+    this.gameIsOver = function() {
+        this.showGameOver();
+    }
+}
+
 //External functions for menu buttons (start, restart, sounds...)
 
 function gameStart() {
@@ -127,6 +250,22 @@ function gameStart() {
         gameIsOn = true;
         var game = new SimonGame()
         game.init();
+    }
+}
+
+function versusStart() {
+    let versusInstructions = '<p>This versus game is for 2 players. Each level starts with Player 1 creating a sequence the same length as the level (1 at level 1, 2 at level 2...). Then it\'s Player 2\'s turn to recreate the same pattern. If they succeed, roles are changed, and now it\'s Player 2 who initiates the sequence and Player 1 who needs to recreate the pattern. If both succeed, the level increases, and so the length of the pattern. The game continues until one of the players does not succeed.</p>'
+
+    let counter = document.getElementById('counter');
+    counter.style.fontSize = '25px';
+    counter.style.lineHeight = 'inherit';
+
+    if (gameIsOn == false) {
+        gameIsOn = true;
+        var game = new SimonVersus();
+        game.init();
+        let instructions = document.getElementById('instructions');
+        instructions.innerHTML = versusInstructions;
     }
 }
 
@@ -153,6 +292,10 @@ restart.addEventListener('click', reload);
 //Start button, starts new game
 let start = document.getElementById('start')
 start.addEventListener('click',gameStart)
+
+//Start button, starts new game
+let versus = document.getElementById('versus')
+versus.addEventListener('click',versusStart)
 
 //Sounds button changes appearance and sets variable soundsStatus true or false.
 let soundButton = document.getElementById('noSounds');
